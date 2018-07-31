4c98c659f63191cef6c70d3c08b6dada430db390
NullDev (Chrus) <shadow@nulldev.org>
2018-08-01 01:36:56 +0200

    Removed commands from main script

--- src/main.js
+++ src/main.js
@@ -50,77 +50,16 @@ client.on("message", message => {
 
         let commandHandler = require(path.join(commandDir, command));
 
-        commandFile.run(client, message, args, function(err){
+        commandHandler.run(client, message, args, function(err){
             if (err){
                 message.channel.send(
                     "Sorry, there has been an error =(\n\n" +
-                    "Please ask @371724846205239326 for help."
+                    "Please ask <@371724846205239326> for help."
                 );
                 log.error(err);
             }
         });
     }
-
-    /*let content = msg.content,
-        author  = msg.author,
-        chan    = msg.channel,
-        log     = config.log_channel_id;
-
-    if (author.id != client.user.id && content.startsWith(config.command_prefix)){
-        let command = content.substr(1, content.length -1).split(" ")[0];
-
-        if (content.split(" ")[1]){
-            let user = content.split(" ")[1];
-            switch(command){
-                case "slap":
-                    chan.send(
-                        `*slaps ${user} around a bit with a large trout*`
-                    );
-                    break;
-                case "kaffee":
-                    chan.send(
-                        `*serviert ${user} einen frisch gebrühten Kaffee aus aromatisch hochwertigen und laktosefreien Kaffeebohnen, die nach dem Vorbild der kolonisierten Ausbeutung herangewachsen und importiert worden sind; Prost!*`
-                    );
-                    break;
-                case "bier":
-                    chan.send(
-                        `*schenkt ein kühles Blondes an ${user} aus.*`
-                    );
-                    break;
-                case "kiss":
-                    chan.send(
-                        `*${author}, (づ｡◕‿‿◕｡)づ 。。・゜゜・。。・゜:heart: ${user} :heart:*`
-                    );
-                    break;
-                case "hug":
-                    chan.send(
-                        `*${author}, ♡:heart:♡:hearts: ${user} :hearts:♡:heart:♡:hearts:*`
-                    );
-                    break;
-                case "döner":
-                    chan.send(
-                        `*Döner für ${user} auf ${author}s Nacken!*`
-                    );
-                    break;
-                case "hawaiipizza":
-                    chan.send(
-                        `*blickt verschämt auf die Hawaiipizza und übergibt diese an ${user}.*`
-                    );
-                    break;
-                case "fiege":
-                    chan.send(
-                        `*reicht ${user} ein kühles Fiege Bier und genießt zusammen mit ihm das Plopp.*`
-                    );
-                    break;
-                case "umtrunk":
-                    let bier = content.split(' ')[2];
-                    chan.send(
-                        `*teilt mit ${user} ein kühles ${bier} und genießt diesen Moment inning.*`
-                    );
-                    break;
-              }
-          }
-      }*/
 });
 
 log.info("Attempting token login...");
