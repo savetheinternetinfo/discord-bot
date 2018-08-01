3e3d803f68db6cba94123534eea607f513901122
NullDev (Chris) <shadow@nulldev.org>
2018-08-01 14:48:41 +0200

    VERY IMPURTAN CHANGES YES

--- src/commands/pedalo.js
+++ src/commands/pedalo.js
@@ -3,7 +3,7 @@
 exports.run = (client, message, args, callback) => {
     let user   = args[0] || message.author;
     message.channel.send(
-        `<:pedalo:473114268435349504> <:pedalo:473114268435349504> <:pedalo:473114268435349504>*${user} fährt mit seinem Pedalo durch den Channel und presst sich dabei einen Monsterenergy. Lit.* :ok_hand: <:pedalo:473114268435349504> <:pedalo:473114268435349504> <:pedalo:473114268435349504>`
+        `<:pedalo:473114268435349504> *${user} fährt mit seinem Pedalo durch den Channel und presst sich dabei 1 Monsterle. Lit.* :ok_hand: <:pedalo:473114268435349504>`
     );
     callback();
 };
